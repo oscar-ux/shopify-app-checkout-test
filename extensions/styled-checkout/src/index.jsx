713931e19ba9c372401e96aadd85301ba6d7000a
Oscar Flores <oscar.uix@gmail.com>
2022-11-12 00:20:03 -0500

    first commit

---
+++ extensions/styled-checkout/src/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import  {render, TextField, useApplyMetafieldsChange, useMetafield } from '@shopify/checkout-ui-extensions-react';
+
+render('Checkout::Dynamic::Render', () => <App />);
+
+function App() {
+  const METAFIELD_NAMESPACE = 'RESIDENT_ID_APP';
+  const METAFIELD_KEY = 'resident_id';
+
+  const [error, setError] = useState(false);
+  const updateMetafield = useApplyMetafieldsChange();
+
+  const residentIdState = useMetafield({
+    namespace: METAFIELD_NAMESPACE,
+    key: METAFIELD_KEY,
+  });
+
+  const handleFieldChange = (value) => {
+    updateMetafield(
+         {
+           type: 'updateMetafield',
+           namespace: METAFIELD_NAMESPACE,
+           key: METAFIELD_KEY,
+           valueType: 'string',
+           value: value
+         }
+       );
+   };
+
+    return (
+      <TextField
+        label='Resident ID'
+        value={residentIdState?.value}
+        error={error? 'Please provide a valid ID': false}
+        onChange={handleFieldChange}
+         />
+      );
+
+}
